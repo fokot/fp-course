7abe72647523aac9d61805276e85265e67d32802
fokot <frantisek.kocun@gmail.com>
2018-10-02 16:57:30 +0200

    List

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -280,7 +280,8 @@ find _ Nil = Empty
 lengthGT4 ::
   List a
   -> Bool
-lengthGT4 a = length (take 4 a) == 4
+lengthGT4 (_ :. _ :. _ :. _ :. _) = True
+lengthGT4 _ = False
 
 -- | Reverse a list.
 --
